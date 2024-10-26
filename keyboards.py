4bde9cf7a8d0628b46dcc6b80732863d9e52523d
Egor Ralovich <vonkad002@gmail.com>
2024-10-27 00:00:08 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -3,22 +3,13 @@ from aiogram import types
 from lang import lang
 
 
-become_driver_kb = [
-        [types.KeyboardButton(text='Стать водителем')]
-    ]
-become_driver_kb = types.ReplyKeyboardMarkup(keyboard=become_driver_kb, resize_keyboard=True)
-
 def cancel_kb(lang_p):
     cancel_kb = [
         [types.KeyboardButton(text=lang['in_main_menu'][lang_p])],
     ]
     return types.ReplyKeyboardMarkup(keyboard=cancel_kb, resize_keyboard=True)
 
-shift_out_kb = [
-        [types.KeyboardButton(text="Поставить статус занят")],
-        [types.KeyboardButton(text="Закрыть смену"), types.KeyboardButton(text="Отправить геолокацию", request_location=True)]
-]
-shift_out_kb = types.ReplyKeyboardMarkup(keyboard=shift_out_kb, resize_keyboard=True)
+
 
 join_kb = [
         [types.KeyboardButton(text="Участвовать")],
